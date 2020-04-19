a04d3acaec6c2f65177d7c34fcd3b892589b5772
Toni Kaplan <tonarie.k@gmail.com>
2020-04-19 12:17:19 -0400

    add firebase

--- readMe.md
+++ readMe.md
@@ -1,4 +1,5 @@
 https://appdividend.com/2018/03/18/how-to-setup-typescript-with-webpack-4/
 https://webpack.js.org/guides/development/
+https://stackoverflow.com/questions/37403747/firebase-permission-denied
 
 http://localhost:8080/
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,71 @@
-// function checkIfInCharts() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4) {
-//       if (this.status == 200) {
-//         const reponse = JSON.parse(this.responseText);
-//         if (reponse && reponse.found !== undefined) {
-//           if (reponse.found && reponse.position) {
-//             document.getElementById("answer").innerHTML = "YES";
-//             document.getElementById("info").innerHTML =
-//               "<a href='https://www.officialcharts.com/charts/' target='_blank'>Its at #" +
-//               reponse.position +
-//               "</a>";
-//           } else {
-//             document.getElementById("answer").innerHTML = "No";
-//             document.getElementById("info").innerHTML = "What a bummer.";
-//           }
-//         }
-//       }
-//       document.getElementById("loading-container").style.display = "none";
-//       document.getElementById("results-container").style.display = "flex";
-//     }
-//   };
-//   xhttp.open(
-//     "GET",
-//     "http://127.0.0.1:8000/charts/singles?artist=Killers&title=Mr%20Brightside",
-//     // "https://1uqnvk9yy3.execute-api.us-east-1.amazonaws.com/api/charts/singles?artist=Killers&title=Mr%20Brightside",
-//     true
-//   );
-//   xhttp.send();
+import "firebase/database";
+import "firebase/auth";
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFoJgJdot1w5nlqLty8wBvWjtFZgSQAC8",
+  authDomain: "mrbrightside-f7929.firebaseapp.com",
+  databaseURL: "https://mrbrightside-f7929.firebaseio.com",
+  projectId: "mrbrightside-f7929",
+  storageBucket: "mrbrightside-f7929.appspot.com",
+  messagingSenderId: "606184561903",
+  appId: "1:606184561903:web:9b319fcf4dec1b3e5f0f1a",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function checkIfInCharts() {
-  console.log("checkIfInCharts");
+  const db = firebase.database();
+  const ref = db.ref();
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      firebase
+        .database()
+        .ref("songs/" + "killers_mrbrightside")
+        .once("value")
+        .then(function (snapshot) {
+          const response = snapshot.val();
+          console.log({ response });
+
+          if (response.position !== undefined) {
+            const date = new Date(0);
+            date.setUTCSeconds(response.updated_at / 1000);
+
+            document.getElementById("answer").innerHTML = "YES";
+            document.getElementById("info").innerHTML =
+              "<a href='https://www.officialcharts.com/charts/' target='_blank'>Its at #" +
+              response.position +
+              "</a>";
+            document.getElementById("updated_at").innerHTML =
+              "Last updated at " + date.toLocaleString();
+          } else {
+            document.getElementById("answer").innerHTML = "No";
+            document.getElementById("info").innerHTML = "What a bummer.";
+          }
+          document.getElementById("loading-container").style.display = "none";
+          document.getElementById("results-container").style.display = "flex";
+        });
+    } else {
+      // User is signed out.
+      // do nothing
+    }
+  });
+
+  // return firebase
+  //   .database()
+  //   .ref("brightside")
+  //   .once("value")
+  //   .then(function (snapshot) {
+  //     console.log(snapshot.val());
+  //   });
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      console.log("fb", { error });
+    });
 }
 
 document.addEventListener("DOMContentLoaded", checkIfInCharts);
