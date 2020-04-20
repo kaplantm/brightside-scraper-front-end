91f105c1811f97f26c11b7ff0933d86c44b952e5
Toni Kaplan <tonarie.k@gmail.com>
2020-04-20 19:56:24 -0400

    use json file

--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,58 @@
-import "firebase/database";
-import "firebase/auth";
-import * as firebase from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAFoJgJdot1w5nlqLty8wBvWjtFZgSQAC8",
-  authDomain: "mrbrightside-f7929.firebaseapp.com",
-  databaseURL: "https://mrbrightside-f7929.firebaseio.com",
-  projectId: "mrbrightside-f7929",
-  storageBucket: "mrbrightside-f7929.appspot.com",
-  messagingSenderId: "606184561903",
-  appId: "1:606184561903:web:9b319fcf4dec1b3e5f0f1a",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-let hasTimedOut = false;
-const maxTimeInMillis = 5000;
-
 function checkIfInCharts() {
-  setTimeout(() => {
-    hasTimedOut = true;
-    document.getElementById("loading-container").style.display = "none";
-    document.getElementById("results-container").style.display = "flex";
-  }, maxTimeInMillis);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        const reponse = JSON.parse(this.responseText);
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (!hasTimedOut) {
-      document.getElementById("error").innerHTML = "";
-      if (user) {
-        // const songFolder = "songs/test/"; // LOCAL
-        const songFolder = "songs/"; // PROD
-        firebase
-          .database()
-          .ref(songFolder + "killers_mrbrightside")
-          .once("value")
-          .then(function (snapshot) {
-            const response = snapshot.val();
-            renderBasedOnResponse(response);
-          });
+        if (reponse && Object.keys(reponse).indexOf("position") !== -1) {
+          renderBasedOnBrightsideData(reponse);
+        } else {
+          displayError();
+        }
       } else {
-        // User is signed out.
-        // do nothing
+        displayError();
       }
     }
-  });
-
-  firebase
-    .auth()
-    .signInAnonymously()
-    .catch(function (error) {
-      console.log("fb", { error });
-    });
+  };
+  xhttp.open(
+    "GET",
+    "https://s3.amazonaws.com/ismrbrightsidestillintheukcharts.com/assets/brightside.json",
+    // "./assets/brightside.json",
+    true
+  );
+  xhttp.send();
 }
 
-function renderBasedOnResponse(response) {
-  if (response.position !== undefined) {
-    const date = new Date(0);
-    date.setUTCSeconds(response.updated_at / 1000);
+function displayError() {
+  document.getElementById("loading-container").style.display = "none";
+  document.getElementById("results-container").style.display = "flex";
+}
 
+function renderBasedOnBrightsideData(brightsideData) {
+  document.getElementById("error").innerHTML = "";
+
+  const { position, updated_at } = brightsideData;
+  if (brightsideData.position) {
     document.getElementById("answer").innerHTML =
-      "YES, " +
-      "<a href='https://www.officialcharts.com/charts/' target='_blank'>#" +
-      response.position +
+      "YES, <a href='https://www.officialcharts.com/charts/' target='_blank'>#" +
+      position +
       "</a>";
     document.getElementById("info").innerHTML = "";
     document.getElementById("updated_at").innerHTML =
-      "Updated: " + date.toLocaleString();
+      "Updated: " + getFormattedDateFromEpochTime(updated_at);
   } else {
     document.getElementById("answer").innerHTML = "No";
     document.getElementById("info").innerHTML = "still a bop tho";
   }
   document.getElementById("loading-container").style.display = "none";
   document.getElementById("results-container").style.display = "flex";
+}
+
+function getFormattedDateFromEpochTime(updated_at) {
+  const date = new Date(0);
+  date.setUTCSeconds(updated_at / 1000);
+  return date.toLocaleString();
 }
 
 document.addEventListener("DOMContentLoaded", checkIfInCharts);
