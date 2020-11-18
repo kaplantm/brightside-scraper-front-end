54a03743865c8c93bd0cf79554af71ae5de52b1a
Toni Kaplan <tonarie.k@gmail.com>
2020-11-17 20:15:30 -0500

    updated at link

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ function checkIfInCharts() {
         if (reponse && Object.keys(reponse).indexOf("position") !== -1) {
           renderBasedOnBrightsideData(reponse);
         } else {
-          displayError();
+          if (reponse && reponse.updated_at) {
+            displayError(reponse.updated_at);
+          }
         }
       } else {
         displayError();
@@ -24,29 +26,35 @@ function checkIfInCharts() {
   xhttp.send();
 }
 
-function displayError() {
+function displayError(updated_at?: string) {
   document.getElementById("loading-container").style.display = "none";
   document.getElementById("results-container").style.display = "flex";
+  document.getElementById(
+    "updated_at"
+  ).innerHTML = ` <a href='https://www.officialcharts.com/charts/' target='_blank'>Updated: ${
+    updated_at ? getFormattedDateFromEpochTime(updated_at) : "unknown"
+  }</a>`;
 }
 
 function renderBasedOnBrightsideData(brightsideData) {
   document.getElementById("error").innerHTML = "";
 
   const { position, updated_at } = brightsideData;
-  if (brightsideData.position) {
-    document.getElementById("answer").innerHTML =
-      "YES, <a href='https://www.officialcharts.com/charts/' target='_blank'>#" +
-      position +
-      "</a>";
+
+  if (position) {
+    document.getElementById("answer").innerHTML = `YES, #${position}</a>`;
     document.getElementById("info").innerHTML = "";
-    document.getElementById("updated_at").innerHTML =
-      "Updated: " + getFormattedDateFromEpochTime(updated_at);
   } else {
     document.getElementById("answer").innerHTML = "No";
     document.getElementById("info").innerHTML = "still a bop tho";
   }
   document.getElementById("loading-container").style.display = "none";
   document.getElementById("results-container").style.display = "flex";
+  document.getElementById(
+    "updated_at"
+  ).innerHTML = ` <a href='https://www.officialcharts.com/charts/' target='_blank'>Updated: ${getFormattedDateFromEpochTime(
+    updated_at
+  )}</a>`;
 }
 
 function getFormattedDateFromEpochTime(updated_at) {
